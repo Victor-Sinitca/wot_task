6a6833a7dafd5ff5711e41cc218ef7f7af4d22b5
Victor Sinitsa <Victor.sinitca@gmail.com>
2021-10-26 20:17:40 +0300

    92

--- src/sand/ex.js
+++ src/sand/ex.js
@@ -1,5 +1,24 @@
+let a = {
+    k:'v',
+    a: 'r'
+};
+a[Symbol.iterator] = function* (){
+    let keys = Object.keys(a);
+    for (let i=0; i<keys.length;i++)
+        yield([keys[i], a[keys[i]]]);
+    return;
+}
 
-let el1={
-    str:"12121",
-    str1:"12121",
+for (let [k,v] of a){
+    console.log(k, v);
+}
+
+a[Symbol.iterator] = function* (){
+    let keys = Object.keys(a);
+    let i=0
+   return {
+        next(){
+            let value
+        }
+   }
 }
